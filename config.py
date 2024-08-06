598575aeeda46df5bb40a956d3c4496e174cba0a
newanimeshows <artificialartist00@gmail.com>
2024-08-06 21:30:56 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+# Your Telegram bot token
+TOKEN = os.getenv('BOT_TOKEN')  # Replace with your actual token in env of koyeb/hiruko/etc
+
+# The Info Of Owner
+CHANNEL_LINK = 'https://t.me/your_channel'
+GROUP_LINK = 'https://t.me/your_group'
+SOURCE_LINK = 'https://example.com/source'
+OWNER_NAME = 'Your Name'
+OWNER_PROFILE_LINK = 'https://example.com/your-profile'
